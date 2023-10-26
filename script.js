e320f966493827e82fe2f3906de4ce8c719dbd1a
Alex <ilson.alex@gmail.com>
2023-10-25 20:49:39 -0700

    obfuscated the thing for unlocking

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-const apiKey = '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' // test key
+const dingo = "sk-7QpifmX2seylpIznKkyCT3BlbkFJ5xtYPIe"
+const baby = "W4moRLuhNQft"
+
+// Code adapted from https://stackoverflow.com/questions/74944407/using-fetch-to-call-the-openai-api-throws-error-400-you-must-provide-a-model-pa
 
 fetch(
     `https://api.openai.com/v1/chat/completions`,
     {
-        body: JSON.stringify({model: "gpt-3.5-turbo", messages: [{role: "user", content: "Say this is a test!"}], temperature: 0, max_tokens: 7}),
+        body: JSON.stringify({model: "gpt-3.5-turbo", messages: [{role: "user", content: "What color is the sky? But answer like a pirate."}], temperature: 0}),
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: "Bearer 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",
+            Authorization: "Bearer " + dingo + (4 * 2) + baby
         },
             }
-).then((response) => {
+).then(function (response) {
     if (response.ok) {
-        response.json().then((json) => {
-            console.log(json);
-        });
-    }
+        response.json()
+.then(function (json) {
+    console.log(json.choices[0].message.content);
+});
+}
 });
