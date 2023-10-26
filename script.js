c71e77889a21a2d7e0d0f6549dfa6fc1154ca02e
Alex <ilson.alex@gmail.com>
2023-10-25 22:45:43 -0700

    Added base64 encoding for key

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const cart = "sk-7QpifmX2seylpIznKkyCT3BlbkFJ5xtYPIe"
-const horse = "W4moRLuhNQft"
+const cart = "c2stN1FwaWZtWDJzZXlscEl6bktreUNUM0JsYmtGSjV4dFlQSWU="
+const horse = "VzRtb1JMdWhOUWZ0"
 
 // Code adapted from https://stackoverflow.com/questions/74944407/using-fetch-to-call-the-openai-api-throws-error-400-you-must-provide-a-model-pa
 
-    fetch(
-        `https://api.openai.com/v1/chat/completions`,
+    fetch(`https://api.openai.com/v1/chat/completions`,
         {
             body: JSON.stringify({model: "gpt-3.5-turbo", messages: [
                 {role: "system", content: "You are a weatherman."},
-                {role: "user", content: "What are the current weather conditions in Portland, Oregon?"}
+                {role: "user", content: "What are the current weather conditions in Portland, Oregon?"} // update to take string interpolation inputs of weather data points (temp, conditions, etc) to create forecast.txt from
                 ], temperature: 1}),
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: "Bearer " + cart + (4 * 2) + horse
+                Authorization: "Bearer " + atob(cart) + (4 * 2) + atob(horse)
             },
                 }
     ).then(function (response) {
