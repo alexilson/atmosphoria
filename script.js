6602fde6e1a0143b0741e1fa5b8ce22fadb08c99
unknown <lizzybumpers11@gmail.com>
2023-10-26 20:00:27 -0500

    switched the start forms to correct html file

--- script.js
+++ script.js
@@ -33,3 +33,7 @@ function getWeatherText(personality, temp, wind, desc, rain) {
 buttonEl.addEventListener("click", function() {
     getWeatherText("a pirate", 45, "12mph NE", "Partially Cloudy", "42%");
 });
+
+getLocationData() {
+    let locationData = getElementById("")
+}
