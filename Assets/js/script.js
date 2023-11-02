5abb2f0e84d24d8462bbec55aede3c0039904b79
unknown <lizzybumpers11@gmail.com>
2023-11-01 20:43:06 -0500

    got pastResponses to displayed from newest to oldest

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,27 +10,25 @@ function getWeatherText(personality, temp, wind, desc) {
     outputEl.textContent = "Please wait...";
     fetch(`https://api.openai.com/v1/chat/completions`,
         {
-            body: JSON.stringify({
-                model: "gpt-3.5-turbo", messages: [
-                    { role: "system", content: `You are a helpful assistant who speaks like ${personality}.` },
-                    { role: "user", content: `Write a weather report for these conditions: temperature: ${temp} wind: ${wind} description: ${desc}` }
-                ], temperature: 1
-            }),
+            body: JSON.stringify({model: "gpt-3.5-turbo", messages: [
+                {role: "system", content: `You are a helpful assistant who speaks like ${personality}.`},
+                {role: "user", content: `Write a weather report for these conditions: temperature: ${temp} wind: ${wind} description: ${desc}`}
+                ], temperature: 1}),
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 Authorization: "Bearer " + atob(cart) + (4 * 2) + atob(horse)
             },
-        }
+                }
     ).then(function (response) {
         if (response.ok) {
             response.json()
-                .then(function (json) {
-                    outputEl.textContent = json.choices[0].message.content;
-                    setLocalStorage(json.choices[0].message.content)
-                    return json;
-                });
-        }
+    .then(function (json) {
+        outputEl.textContent =json.choices[0].message.content;
+        setLocalStorage(json.choices[0].message.content)
+        return json;
+    });
+    }
     });
 }
 
@@ -40,42 +38,42 @@ function getWeatherFromZip(location, units, accent) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${weatherApiKey}&units=${units}`;
 
     fetch(apiUrl)
-        .then((response) => {
-            if (response.status !== 200) {
-                console.error(`Error: ${response.status}`);
-                return;
-            }
-            return response.json();
-        })
-        .then((data) => {
-            if (data) {
-                const temp = data.main.temp;
-                let windSpeed = data.wind.speed;
-                let windDirection = data.wind.deg;
-                const wind = (windSpeed, windDirection)
-                const desc = data.weather[0].description
-                const rain = data.rain
+    .then((response) => {
+      if (response.status !== 200) {
+        console.error(`Error: ${response.status}`);
+        return;
+      }
+      return response.json();
+    })
+    .then((data) => {
+        if (data){
+            const temp = data.main.temp;
+            let windSpeed = data.wind.speed;
+            let windDirection = data.wind.deg;
+            const wind = (windSpeed, windDirection)
+            const desc = data.weather[0].description
+            const rain = data.rain
 
-                getWeatherText(accent, temp, wind, desc)
+            getWeatherText(accent, temp, wind, desc)
 
-                console.log('Temperature:', temp)
-                console.log('Wind', wind)
-                console.log('Description', desc)
-                console.log('Rain Chance:', rain)
-                console.log(data)
-            } else {
-                console.log('No weather data available')
-            }
+            console.log('Temperature:', temp)
+            console.log('Wind', wind)
+            console.log('Description', desc)
+            console.log('Rain Chance:', rain)
+            console.log(data)
+        } else {
+            console.log('No weather data available')
+        }
 
-        })
-        .catch((error) => {
-            console.error('Fetch error:', error);
-        });
-
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+ 
 }
 
 function setLocalStorage(forecastResponse) {
-
+    
     // create response history object in correct scope
     const responseHistory = {}
 
@@ -85,7 +83,7 @@ function setLocalStorage(forecastResponse) {
         const parsedresponseHistory = JSON.parse(pastResponsesLS);
         Object.assign(responseHistory, parsedresponseHistory);  // code from Xpert
     }
-
+    
     // create timestamp of current time
     const timestamp = dayjs().format('dddd, MMMM D[th], YYYY [at] h[:]mm[:]s a')
 
@@ -108,45 +106,59 @@ function displayPastResponses() {
         const responseKeys = Object.keys(parsedResponseHistory);
         const responseCount = responseKeys.length;
 
-        // removes oldest response
+        // code from XPERT
         if (responseCount > 5) {
-            const responsesToRemove = responseCount - 5;
-            for (let i = 0; i < responsesToRemove; ++i) {
-                delete parsedResponseHistory[responseKeys[i]];
+            let startIndex = responseCount - 5;
+            for (let i = responseCount - 1; i >= startIndex; --i) {
+                let key = responseKeys[i]
+                let responseTimestamp = key;
+                let responseText = parsedResponseHistory[key];
+
+                const responseTimeStampEl = document.createElement('div');
+                responseTimeStampEl.textContent = responseTimestamp;
+
+                const responseTextEl = document.createElement('div');
+                responseTextEl.textContent = responseText;
+
+                // console.log(responseTimestamp + "\n" + responseText)
+
+                responsesEl.append(responseTimeStampEl);
+                responsesEl.append(responseTextEl);
+
+                if (i === startIndex) {
+                    break;
+                }
             }
-        }
+        } else {
+            for (let i = responseCount - 1; i >= 0; i--) { // Iterate over all response keys
+                let key = responseKeys[i];
+                let responseTimestamp = key;
+                let responseText = parsedResponseHistory[key];
 
-        // displays the past responses
-        for (let key in parsedResponseHistory) {
-            let responseTimestamp = key;
-            let responseText = parsedResponseHistory[key];
+                const responseTimeStampEl = document.createElement('div');
+                responseTimeStampEl.textContent = responseTimestamp;
 
-            // creates a <div> to display a past response and the timestamp
-            const responseTimeStampEl = document.createElement('div');
-            responseTimeStampEl.textContent = responseTimestamp;
+                const responseTextEl = document.createElement('div');
+                responseTextEl.textContent = responseText;
 
-            const responseTextEl = document.createElement('div');
-            responseTextEl.textContent = responseText;
-
-            // console.log(responseTimestamp + "\n" + responseText)
-
-            responsesEl.append(responseTimeStampEl);
-            responsesEl.append(responseTextEl);
+                responsesEl.prepend(responseTextEl); // Prepend the elements to display the latest response on top
+            }
         }
     }
 };
 
-// assisted from XPERT Learning Assistant
-function getParametersFromUrl() {
-    let urlString = window.location.search;
-    let urlParams = new URLSearchParams(urlString);
+// let location = qParams
+// let accent = accentParam
 
-    // gets the ZipCode and Accent from the URL Search
-    let qParam = urlParams.get('q');
-    let accentParam = urlParams.get('accent');
-    console.log(qParam);
-    console.log(accentParam);
-    getWeatherFromZip(qParam, 'imperial', accentParam)
+// assisted from XPERT Learning Assistant
+function getParametersFromUrl () {
+let urlString = window.location.search;
+let urlParams = new URLSearchParams(urlString);
+let qParam = urlParams.get('q');
+let accentParam = urlParams.get('accent');
+console.log(qParam);
+console.log(accentParam);
+getWeatherFromZip(qParam,'imperial', accentParam)
 }
 getParametersFromUrl();
 displayPastResponses();
