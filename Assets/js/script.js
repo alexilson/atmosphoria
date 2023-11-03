8a8a00a9cfad7df112b3ac81eea80eebe881906d
Alex <ilson.alex@gmail.com>
2023-11-02 17:21:16 -0700

    Removed code that had been commented

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -87,9 +87,6 @@ function setLocalStorage(forecastResponse) {
         Object.assign(responseHistory, parsedresponseHistory);  // code from Xpert
     }
 
-    // create timestamp of current time
-    // const timestamp = dayjs().format('dddd, MMMM D[th], YYYY [at] h[:]mm[:]s a')
-
     // add forecast response with timestamp as the key
     responseHistory[currentTimestamp] = forecastResponse;
 
@@ -150,9 +147,6 @@ function displayPastResponses() {
         }
     }
 };
-
-// let location = qParams
-// let accent = accentParam
 
 // assisted from XPERT Learning Assistant
 function getParametersFromUrl() {
