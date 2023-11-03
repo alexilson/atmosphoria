9f68eac4462f92bc6a3ed74d6f88ca137c25d6f7
unknown <lizzybumpers11@gmail.com>
2023-11-02 20:45:47 -0500

    added screenshots and edited readme

--- Assets/js/start.js
+++ Assets/js/start.js
@@ -1,25 +1,16 @@
-// function to get the zip code to be returned in a variable that can be turned into a parameter for the URL
 function getLocationData() {
     let locationEL = document.getElementById("location");
-    let locationValue = locationEL.value.trim();
+    let locationValue = locationEL.value;
     console.log(locationValue);
-
-    //eror handling to ensure that a valid zip code is being entered and calling a modal upon an error
-    const zipCodePattern = /^\d{5}$/;
-
-    if (zipCodePattern.test(locationValue)) {
-        return locationValue
-    } else {
-        showErrorModal("Please enter a valid 5 digit zip code")
-        return null
-    }
+    // getWeatherFromZip(locationValue)
+    return locationValue;
 }
 
 document.getElementById("startForm").addEventListener("submit", function (event) {
     event.preventDefault();
+    getLocationData();
 });
 
-//function to get the accent to be returned in a variable that can be turned into a paramater for the url
 function getSelectedAccent() {
     let dropdown = document.getElementById("accent")
     let selectedAccent = dropdown.value
@@ -27,42 +18,11 @@ function getSelectedAccent() {
     return selectedAccent;
 }
 
-//button that sends the accent and zip code parameters for the creation of the url with error handling to prevent it from working in the event of an error with the zipcode
 let clickButton = document.getElementById("btn")
-clickButton.addEventListener("click", function () {
+clickButton.addEventListener("click", function() {
     event.preventDefault();
-
     let inputLocation = getLocationData();
-    if (inputLocation !== null) {
-        let inputAccent = getSelectedAccent();
-        let urlQuery = `./index.html?q=${inputLocation}&accent=${inputAccent}`;
-        window.location.href = urlQuery;
-    }
+    let inputAccent = getSelectedAccent();
+    let urlQuery = `./index.html?q=${inputLocation}&accent=${inputAccent}`; 
+    window.location.href = urlQuery;
 })
-
-
-//dynamically creates a modal that displays when the zip code is invalid
-function showErrorModal(message) {
-    const modalContainer = document.getElementById("modalContainer")
-    const errorModal = document.createElement("div")
-    errorModal.className = "modal"
-    const modalContent = document.createElement("div")
-    modalContent.className = "modal-content"
-    const closeBtn = document.createElement("span")
-    closeBtn.className = "close"
-    closeBtn.innerHTML = "&times;"
-    const errorMsg = document.createElement("p")
-    errorMsg.textContent = message
-
-    modalContent.appendChild(closeBtn)
-    modalContent.appendChild(errorMsg)
-    errorModal.appendChild(modalContent)
-    modalContainer.appendChild(errorModal)
-
-    errorModal.style.display = "block"
-
-    closeBtn.onclick = function () {
-        errorModal.style.display = "none"
-    }
-
-}
