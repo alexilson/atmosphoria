af6780b667d07821212ad32f3157e06d2a171f5e
MarkRBishop <mrb.8@hotmail.com>
2023-11-04 22:49:49 -0500

    commented on functions in response.js and script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,6 +10,7 @@ function getLocationData() {
     if (zipCodePattern.test(locationValue)) {
         return locationValue
     } else {
+        //provides the message to be displayed in the the showErrorModal()
         showErrorModal("Please enter a valid 5 digit zip code")
         return null
     }
@@ -43,6 +44,7 @@ clickButton.addEventListener("click", function () {
 
 //dynamically creates a modal that displays when the zip code is invalid
 function showErrorModal(message) {
+    //creates the different elements and gives them class names
     const modalContainer = document.getElementById("modalContainer")
     const errorModal = document.createElement("div")
     errorModal.className = "modal"
@@ -50,10 +52,12 @@ function showErrorModal(message) {
     modalContent.className = "modal-content"
     const closeBtn = document.createElement("span")
     closeBtn.className = "close"
+    //creates a close "x" button
     closeBtn.innerHTML = "&times;"
     const errorMsg = document.createElement("p")
     errorMsg.textContent = message
 
+    //nests the different elements inside each other for the modal
     modalContent.appendChild(closeBtn)
     modalContent.appendChild(errorMsg)
     errorModal.appendChild(modalContent)
@@ -61,6 +65,7 @@ function showErrorModal(message) {
 
     errorModal.style.display = "block"
 
+    //hides the modal when the button is clicked
     closeBtn.onclick = function () {
         errorModal.style.display = "none"
     }
